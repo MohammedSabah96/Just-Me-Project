db2d6e5693b32bc0a0a1bf282f6798f81fc9d32f
MohammedSabah96 <mohammedsabah455@gmail.com>
2020-04-15 08:13:01 +0300

    Added 404 page

--- mvc/controllers/index.js
+++ mvc/controllers/index.js
@@ -7,10 +7,23 @@ const getHomePage = (req, res) => {
 
 const getBlogPost = ({ params }, res) => {
   let post = postData.find((val) => val.id == params.postid);
+  if (!post) {
+    res.redirect("/404");
+  }
   res.render("post", { title: post.title, post: post });
+};
+
+const get404 = (req, res) => {
+  res.render("404", { title: "404 -I couldn't find that page..." });
+};
+
+const redirect404 = (req, res) => {
+  res.redirect("/404");
 };
 
 module.exports = {
   getHomePage,
   getBlogPost,
+  get404,
+  redirect404,
 };
