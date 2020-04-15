dbb751a5e4929bbc2e0d84d8eeec7001e656a4be
MohammedSabah96 <mohammedsabah455@gmail.com>
2020-04-15 09:42:17 +0300

    Added sidebar-right dynamically

--- mvc/controllers/index.js
+++ mvc/controllers/index.js
@@ -1,8 +1,11 @@
 const data = require("../../data");
 const postData = data.postData;
+const uniqueTags = data.uniqueTags;
+
+const recentPostsAmount = 3;
 
 const getHomePage = (req, res) => {
-  res.render("index", { title: "just Me", posts: postData, active:"index" });
+  res.render("index", { title: "just Me", posts: postData, active: "index" });
 };
 
 const getBlogPost = ({ params }, res) => {
@@ -10,11 +13,22 @@ const getBlogPost = ({ params }, res) => {
   if (!post) {
     res.redirect("/404");
   }
-  res.render("post", { title: post.title, post: post });
+  res.render("post", {
+    title: post.title,
+    post: post,
+    uniqueTags: uniqueTags,
+    recentPosts: postData.slice(0, recentPostsAmount),
+    categoryData: data.categoryData,
+  });
 };
 
 const get404 = (req, res) => {
-  res.render("404", { title: "404 -I couldn't find that page..." });
+  res.render("404", {
+    title: "404 -I couldn't find that page...",
+    uniqueTags: uniqueTags,
+    recentPosts: postData.slice(0, recentPostsAmount),
+    categoryData: data.categoryData,
+  });
 };
 
 const redirect404 = (req, res) => {
@@ -22,12 +36,12 @@ const redirect404 = (req, res) => {
 };
 
 const getAbout = (req, res) => {
-  res.render("about", {title:"About Me", active:"about"})
-}
+  res.render("about", { title: "About Me", active: "about" });
+};
 
 const getContact = (req, res) => {
-  res.render("contact", {title:"Contact", active:"contact"})
-}
+  res.render("contact", { title: "Contact", active: "contact" });
+};
 
 module.exports = {
   getHomePage,
